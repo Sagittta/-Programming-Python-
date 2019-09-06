98236dc0e3433a5bbf32ff33098a292ea8d2d5ab
Sagittta <js1verj@gmail.com>
2019-09-06 15:00:22 +0900

    여유, 보통, 혼잡 계산

---
+++ 모듈/loose_normal_crowded.py
@@ -0,0 +1,21 @@
+# 버스 여유, 보통, 혼잡
+# 탑승객, 하차객 입력 받자
+# 0 이상 10 미만 : 여유
+# 10 이상 20 미만 : 보통
+# 20 이상 : 혼잡
+
+while True:
+    in_person = int(input("탑승객 수는? "))
+    out_person = int(input("하차객 수는? "))
+
+sum = in_person - out_person
+print("버스에 있는 사람 수는", sum, "명\n")
+
+if sum < 0:
+    print("오류 발생")
+elif 0 <= sum < 10:
+    print("여유")
+elif 10 <= sum < 20:
+    print("보통")
+else:
+    print("혼잡")
