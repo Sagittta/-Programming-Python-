044b867e2e29ded2e0546fed9503d4787842b287
Sagittta <js1verj@gmail.com>
2019-10-01 10:50:22 +0900

    read text file, encoding

--- VIII. 파일처리/write_text_file1.py
+++ VIII. 파일처리/write_text_file1.py
@@ -1,5 +1,5 @@
-# f = open("file.txt", "w")
-f = open("file.txt", "a")
+# f = open("file.txt", "w")     # 덮어쓰기
+f = open("file.txt", "a")       # 뒤에 이어서 쓰기
 
 f.write("Hello")
 f.write("\n")       # 이거 해줘야 줄바꿈이 됨.
