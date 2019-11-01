7a40a065efa3955b7101ba54eca70138a6b479ed
Sagittta <js1verj@gmail.com>
2019-11-01 13:30:47 +0900

    TicTacToe - for문 추가

--- IX. 프로젝트/tictactoe.py
+++ IX. 프로젝트/tictactoe.py
@@ -10,7 +10,7 @@ class TicTacToe:
             # else:
             #     self.current_turn = "o"
             # 위 4문장과 같은 의미.
-            self.current_turn = (" X " if self.current_turn == " O " else " O ")
+            self.current_turn = (" X    " if self.current_turn == " O " else " O ")
             self.board[(row * 3) + col] = self.current_turn
 
     def get(self, row, col):
@@ -19,21 +19,13 @@ class TicTacToe:
     def check_winner(self):
         check = self.current_turn
 
-        # -
-        if self.get(0, 0) == self.get(0, 1) == self.get(0, 2) == check:
-            return check
-        if self.get(1, 0) == self.get(1, 1) == self.get(1, 2) == check:
-            return check
-        if self.get(2, 0) == self.get(2, 1) == self.get(2, 2) == check:
-            return check
-
-        # |
-        if self.get(0, 0) == self.get(1, 0) == self.get(2, 0) == check:
-            return check
-        if self.get(0, 1) == self.get(1, 1) == self.get(2, 1) == check:
-            return check
-        if self.get(0, 2) == self.get(1, 2) == self.get(2, 2) == check:
-            return check
+        for i in range(3):
+            # -
+            if self.get(i, 0) == self.get(i, 1) == self.get(i, 2) == check:
+                return check
+            # |
+            if self.get(0, i) == self.get(1, i) == self.get(2, i) == check:
+                return check
 
         # \
         if self.get(0, 0) == self.get(1, 1) == self.get(2, 2) == check:
