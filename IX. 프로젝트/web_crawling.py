483541e27b9029bf7eb6c7d38e100d49a35a2d58
Sagittta <js1verj@gmail.com>
2019-11-29 14:54:06 +0900

    with urlopen() as response 로 close() 하지 않아도 되도록 변경

--- IX. 프로젝트/web_crawling.py
+++ IX. 프로젝트/web_crawling.py
@@ -7,12 +7,17 @@ from bs4 import BeautifulSoup           #html 구조적으로 변환하자  alt+
 from urllib.request import urlopen      #url에 해당하는 html 가져오자
 
 if __name__ == '__main__':
-    # urlopen 하자
-    response = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=703852&weekday=tue")
+    # # urlopen 하자
+    # data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=703852&weekday=tue")
+    #
+    # # BeautifulSoup 객체 생성하자
+    # soup = BeautifulSoup(data, "lxml")
+    # # print(soup)
+    #
+    # data.close()
 
-    # BeautifulSoup 객체 생성하자
-    soup = BeautifulSoup(response, "lxml")
-    # print(soup)
+    with urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=703852&weekday=tue") as data:
+        soup = BeautifulSoup(data, "lxml")
 
     # 원하는 태그나 속성으로 찾자
     cartoon_titles = soup.find_all("td", attrs={"class": "title"})
@@ -20,7 +25,7 @@ if __name__ == '__main__':
     for title in cartoon_titles[:]:
         t = title.find("a").text
         link = title.find("a").get("href")
-        link = "https://comic.naver.com" + link
+        link = "https://comic.naver.com/" + link
         print(t)
         print(link)
 
--- IX. 프로젝트/web_crawling_daumwebtoon.py
+++ IX. 프로젝트/web_crawling_daumwebtoon.py
@@ -4,8 +4,8 @@ from urllib.request import urlopen
 import json
 
 if __name__ == '__main__':
-    response = urlopen("http://webtoon.daum.net/data/pc/webtoon/view/favorite?timeStamp=1574736026313")
-    response_byte = response.read()
+    with urlopen("http://webtoon.daum.net/data/pc/webtoon/view/favorite?timeStamp=1574736026313") as response:
+        response_byte = response.read()
     response_json = json.loads(response_byte)
     # print(json.loads(response.read()))
     # print(response_json['data']['webtoon']['webtoonEpisodes'][11]['title'])  # data.webtoon.webtoonEpisodes[11].title
